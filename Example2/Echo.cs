f7d7ddce30b369ef3bf669e95c5b328d099b6ce7
yue <yue.chen@socialfuture.io>
2023-10-07 11:09:38 +0800

    添加proto buf支持

--- Example2/Echo.cs
+++ Example2/Echo.cs
@@ -1,14 +1,28 @@
 using System;
 using WebSocketSharp;
 using WebSocketSharp.Server;
+using Texas.Protocol;
+using System.IO;
+using System.Security.Cryptography;
+using Google.Protobuf;
 
 namespace Example2
 {
-  public class Echo : WebSocketBehavior
-  {
-    protected override void OnMessage (MessageEventArgs e)
+    public class Echo : WebSocketBehavior
     {
-      Send (e.Data);
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            EnterRoomRSP enterRoomRSP = new EnterRoomRSP();
+            enterRoomRSP.Roomid = 5;
+
+            byte[] bytes = null;
+            using (MemoryStream rspStream = new MemoryStream())
+            {
+                enterRoomRSP.WriteTo(rspStream);
+                bytes = rspStream.ToArray();
+            }
+
+            Send(bytes);
+        }
     }
-  }
 }
